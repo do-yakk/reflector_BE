bae69dde86d36f6d3b686f13fb3a5511b9ff3b07
je0ng3 <jepark2934@gmail.com>
2024-10-28 14:41:32 +0900

    setup project

---
+++ src/main/java/com/doyak/reflector/ReflectorApplication.java
@@ -0,0 +1,13 @@
+package com.doyak.reflector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReflectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doyak/reflector/ReflectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doyak.reflector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
