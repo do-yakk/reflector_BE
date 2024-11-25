6f69ea20f1a21716010b9772c5803320d6fa0458
iyeongseo <iyeongssseo@gmail.com>
2024-11-25 11:37:09 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/com/doyak/reflector/presentation/UserController.java
+++ src/main/java/com/doyak/reflector/presentation/UserController.java
@@ -10,7 +10,6 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -88,6 +87,14 @@ public class UserController {
             return "login";
         }
         return "home";
+    }
+
+    @PostMapping("logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        session.invalidate();
+
+        return "redirect:/";
     }
 
 }
