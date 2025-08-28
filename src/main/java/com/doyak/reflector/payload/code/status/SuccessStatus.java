d34f2fd80cc67ee11810b8afa8af81a9c25357d3
je0ng3 <jepark2934@gmail.com>
2025-08-29 02:02:05 +0900

    [feat] API 응답 객체 및 성공 코드 추가

---
+++ src/main/java/com/doyak/reflector/payload/code/status/SuccessStatus.java
@@ -0,0 +1,32 @@
+package com.doyak.reflector.payload.code.status;
+
+import org.springframework.http.HttpStatus;
+
+import com.doyak.reflector.payload.code.BaseCode;
+import com.doyak.reflector.payload.dto.ReasonDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    OK(HttpStatus.OK, "COMMON200", "성공입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+
+    @Override
+    public ReasonDTO getReason() {
+        return ReasonDTO.builder()
+                .httpStatus(httpStatus)
+                .isSuccess(true)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
+
---
+++ src/main/java/com/doyak/reflector/payload/dto/ReasonDTO.java
@@ -0,0 +1,17 @@
+package com.doyak.reflector.payload.dto;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ReasonDTO {
+
+    private HttpStatus httpStatus;
+    private final boolean isSuccess;
+    private final String code;
+    private final String message;
+
+}
