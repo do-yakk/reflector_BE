83046a238ba1baca06e60c8f3faf4f004c43f9ba
je0ng3 <jepark2934@gmail.com>
2025-09-06 16:00:41 +0900

    [feat] Post Error Status 추가

--- src/main/java/com/doyak/reflector/payload/code/status/ErrorStatus.java
+++ src/main/java/com/doyak/reflector/payload/code/status/ErrorStatus.java
@@ -12,10 +12,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
 
+	// 공통 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러입니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+	
+    // Post
+	POST_NOT_FOUND(HttpStatus.NOT_FOUND, "POST404", "게시글을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
