28311d756758e3859f29705d775942acaee2024e
je0ng3 <jepark2934@gmail.com>
2025-09-17 19:43:10 +0900

    [feat] block CRUD 로직과 관련된 레이어들 작성

---
+++ src/main/java/com/doyak/reflector/service/BlockService.java
@@ -0,0 +1,91 @@
+package com.doyak.reflector.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.doyak.reflector.converter.BlockConverter;
+import com.doyak.reflector.domain.Block;
+import com.doyak.reflector.domain.CodeBlock;
+import com.doyak.reflector.domain.Post;
+import com.doyak.reflector.domain.TextBlock;
+import com.doyak.reflector.dto.request.BlockRequest;
+import com.doyak.reflector.dto.response.BlockResponse;
+import com.doyak.reflector.payload.code.status.ErrorStatus;
+import com.doyak.reflector.payload.exception.GeneralException;
+import com.doyak.reflector.repository.BlockRepository;
+import com.doyak.reflector.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BlockService {
+
+	private final PostRepository postRepository;
+    private final BlockRepository blockRepository;
+    private final BlockConverter blockConverter; 
+
+    // 코드 블럭 생성  
+    @Transactional
+    public BlockResponse createBlock(BlockRequest request, Long postId) {
+    	Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new GeneralException(ErrorStatus.POST_NOT_FOUND));
+    	int nextOrderIndex = blockRepository.findMaxOrderIndexByPost(post).orElse(0) + 1;
+    	Block block = blockConverter.toBlock(request, nextOrderIndex, post);
+    	Block saved = blockRepository.save(block);
+    	return blockConverter.toResponse(saved);
+    }
+    
+    // 단일 블럭 조회 
+    @Transactional(readOnly = true)
+    public BlockResponse getBlock(Long blockId) {
+        Block block = findBlockById(blockId);
+        return blockConverter.toResponse(block);
+    }
+
+    // 게시글에 속한 블럭 목록 조회 
+    @Transactional(readOnly = true)
+    public List<BlockResponse> getBlocksByPostId(Long postId) {
+        List<Block> blocks = blockRepository.findAllByPostIdOrderByOrderIndex(postId);
+        return blockConverter.toResponseList(blocks);
+    }
+    
+    // 블럭 수정
+    @Transactional
+    public BlockResponse updateBlock(Long blockId, BlockRequest request) {
+        Block block = findBlockById(blockId);
+
+        if (request instanceof BlockRequest.TextCommand textRequest && block instanceof TextBlock textBlock) {
+            updateTextBlock(textBlock, textRequest);
+            return blockConverter.toResponse(textBlock);
+        } else if (request instanceof BlockRequest.CodeCommand codeRequest && block instanceof CodeBlock codeBlock) {
+            updateCodeBlock(codeBlock, codeRequest);
+            return blockConverter.toResponse(codeBlock);
+        } else {
+        	throw new GeneralException(ErrorStatus.UNSUPPORTED_BLOCK_TYPE);
+        }
+    }
+
+    private void updateTextBlock(TextBlock block, BlockRequest.TextCommand request) {
+    	block.update(request.getContent());
+    }
+
+    private void updateCodeBlock(CodeBlock block, BlockRequest.CodeCommand request) {
+        block.update(request.getContent(), request.getLanguage(), request.getPerformTime(), request.getPerformMem());
+    }
+
+
+    // 블럭 삭제 
+    @Transactional
+    public void deleteBlock(Long blockId) {
+        blockRepository.deleteById(blockId);
+    }
+    
+    private Block findBlockById(Long blockId) {
+    	Block block = blockRepository.findById(blockId)
+    		    .orElseThrow(() -> new GeneralException(ErrorStatus.BLOCK_NOT_FOUND));
+    	return block;
+    }
+}
