4ca60876cb52f355a05e6443464010a556d10b55
je0ng3 <jepark2934@gmail.com>
2025-09-17 17:30:33 +0900

    [feat] block 요청/응답 dto 정의

---
+++ src/main/java/com/doyak/reflector/dto/response/BlockResponse.java
@@ -0,0 +1,33 @@
+package com.doyak.reflector.dto.response;
+
+import com.doyak.reflector.domain.enums.Language;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public interface BlockResponse {
+    
+	@Builder
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    @Getter
+	public class Text implements BlockResponse {
+        private String content;
+        private Integer orderIndex;
+    }
+	
+	@Builder
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    @Getter
+    public class Code implements BlockResponse {
+        private String content;
+        private Language language;
+        private double performTime;
+        private double performMem;
+    }
+    
+}
