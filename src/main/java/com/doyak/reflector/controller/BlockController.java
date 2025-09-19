94c74475d78f77a29c9ed638d247ba586e7ba2d6
je0ng3 <jepark2934@gmail.com>
2025-09-19 16:14:51 +0900

    [fix] api 호출을 위한 PathVariable 추가

--- src/main/java/com/doyak/reflector/controller/BlockController.java
+++ src/main/java/com/doyak/reflector/controller/BlockController.java
@@ -42,7 +42,7 @@ public class BlockController {
 	
 	@GetMapping("/{blockId}")
 	@Operation(summary = "해당 블럭 읽기", description = "읽어오길 원하는 블럭 아이디를 입력해주세요.")
-    public ApiResponse<BlockResponse> getBlock(@PathVariable("blockId") Long blockId) {
+    public ApiResponse<BlockResponse> getBlock(@PathVariable("postId") Long postId, @PathVariable("blockId") Long blockId) {
         return ApiResponse.onSuccess(blockService.getBlock(blockId));
     }
 
@@ -54,14 +54,14 @@ public class BlockController {
     
     @PutMapping("/text/{blockId}")
     @Operation(summary = "텍스트 블럭 수정", description = "수정할 텍스트 내용을 입력해주세요.")
-    public ApiResponse<BlockResponse> updateTextBlock(@PathVariable("blockId") Long blockId,
+    public ApiResponse<BlockResponse> updateTextBlock(@PathVariable("postId") Long postId, @PathVariable("blockId") Long blockId,
             											@RequestBody BlockRequest.TextCommand request) {
         return ApiResponse.onSuccess(blockService.updateBlock(blockId, request));
     }
     
     @PutMapping("/code/{blockId}")
     @Operation(summary = "코드 블럭 수정", description = "수정할 코드 내용을 입력해주세요.")
-    public ApiResponse<BlockResponse> updateCodeBlock(@PathVariable("blockId") Long blockId,
+    public ApiResponse<BlockResponse> updateCodeBlock(@PathVariable("postId") Long postId, @PathVariable("blockId") Long blockId,
     													@RequestBody BlockRequest.CodeCommand request) {
         return ApiResponse.onSuccess(blockService.updateBlock(blockId, request));
     }
@@ -69,7 +69,7 @@ public class BlockController {
 
     @DeleteMapping("{blockId}")
 	@Operation(summary = "해당 블럭 삭제", description = "삭제하길 원하는 블럭 아이디를 입력해주세요.")
-    public void deleteBlock(@PathVariable("blockId") Long blockId) {
+    public void deleteBlock(@PathVariable("postId") Long postId, @PathVariable("blockId") Long blockId) {
         blockService.deleteBlock(blockId);
     }
 
