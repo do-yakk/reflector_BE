ff54925893ac0bd557cd72dbf68ccc185ef0dec8
je0ng3 <jepark2934@gmail.com>
2025-09-20 22:00:24 +0900

    [feat] 포스트 단건 조회 시 블록도 함께 반환하도록 수정

--- src/main/java/com/doyak/reflector/repository/PostRepository.java
+++ src/main/java/com/doyak/reflector/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.doyak.reflector.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.doyak.reflector.domain.Post;
@@ -11,4 +14,7 @@ import com.doyak.reflector.domain.User;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByUserOrderByCreatedAtDesc(User user); 
+    
+    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.blocks WHERE p.postId = :postId")
+    Optional<Post> findByIdWithBlocks(@Param("postId") Long postId);
 }
