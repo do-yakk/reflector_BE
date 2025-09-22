30ad365780b16a2564ba177af626bc6862f8421a
je0ng3 <jepark2934@gmail.com>
2025-09-22 22:24:39 +0900

    [fix] Hashtag 생성자 private 처리

--- src/main/java/com/doyak/reflector/domain/Hashtag.java
+++ src/main/java/com/doyak/reflector/domain/Hashtag.java
@@ -9,12 +9,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Builder
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class Hashtag {
 
 	@Id
