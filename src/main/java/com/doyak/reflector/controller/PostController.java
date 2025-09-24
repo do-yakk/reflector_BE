ed5b4aa737a22f03702c95b57ecdb99666e484e9
iyeongseo <iyeongssseo@gmail.com>
2025-09-24 13:42:03 +0900

    [fix] 충돌 문제 해결

--- src/main/java/com/doyak/reflector/controller/PostController.java
+++ src/main/java/com/doyak/reflector/controller/PostController.java
@@ -1,5 +1,7 @@
 package com.doyak.reflector.controller;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
